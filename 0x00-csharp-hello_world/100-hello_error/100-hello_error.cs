16b6894010fddf8b536424b28f1209ba53ee7e2f
aike-s <sagrilo804@gmail.com>
2022-04-11 18:10:05 -0500

    [add] directory, proyect files and readme

---
+++ 0x00-csharp-hello_world/100-hello_error/100-hello_error.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static int Main(string[] args)
+    {
+        var strErr = Console.Error;
+        strErr.WriteLine($"and that piece of art is useful - Dora Korpar, 2015-10-19");
+        return 1;
+    }
+}
